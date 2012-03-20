df87e2b8276784d3491bfe9df1020499e503bc1c
Saiesh N <saiesh.nat@gmail.com>
2012-03-20 21:17:39 +0530

    added rootkit_conf.conf.h

---
+++ rootkit_conf.conf.h
@@ -0,0 +1,7 @@
+static char password[] = "secretpassword" ; 
+static char passwaiter[] = "version" ; 
+static char module_release[] = "release" ;
+static char module_uncover[] = "uncover" ;
+static char hide_proc[] = "hide" ;
+static char unhide_proc[] = "unhide"; 
+
